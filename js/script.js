365e589eed4a2d2be7675e2ec46127dc9e909f96
Kiko Nuñez <kikoland@Federicos-MacBook-Air.local>
2022-11-29 17:08:48 -0700

    adds elements to html body

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://anime-quotes1.p.rapidapi.com/api/quotes/anime?title=One%20Piece",
-	"method": "GET",
-	"headers": {
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://anime-quotes1.p.rapidapi.com/api/quotes/anime?title=One%20Piece",
+// 	"method": "GET",
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+
+$.ajax({
+    url:'https://anime-quotes1.p.rapidapi.com/api/quotes/anime?title=One%20Piece',
+    "headers": {
 		"X-RapidAPI-Key": "2eba11bd42msh3cdfe07ccc61a1cp13ef6bjsn84cf6ca7c03a",
 		"X-RapidAPI-Host": "anime-quotes1.p.rapidapi.com"
 	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+}).then(
+    (data) => {
+     console.log(data);
+    }).catch(
+    (error) => {
+     console.log('bad request', error);
+    })
